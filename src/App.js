063b5e3ab7d5bb58d29aa03c588c422ea8049ef8
jsh-1235 <>
2022-04-01 11:59:34 +0900

    initialize

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// import { FaBeer } from "react-icons/fa";
+// import { AiOutlineCalculator } from "react-icons/ai";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      {/* <div style={{ color: "blue", fontSize: "10rem" }}>
+        <AiOutlineCalculator />
+      </div> */}
+      <Main />
+    </div>
+  );
+}
+
+export default App;

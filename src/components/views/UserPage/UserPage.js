27ee397f23bb2ff2fc7cfebad59a96f075e9764d
jsh-1235 <>
2022-04-07 12:40:41 +0900

    update

--- src/components/views/UserPage/UserPage.js
+++ src/components/views/UserPage/UserPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Table, Button, Typography, Space } from "antd";
+import { Table, Button, Typography, Space, notification, Popover, Checkbox } from "antd";
+import { green } from "@ant-design/colors";
 
 import UpdateModal from "./Sections/UpdateModal";
 import DeleteModal from "./Sections/DeleteModal";
@@ -9,11 +10,12 @@ import { deleteUser, getUser } from "../../../actions/user";
 
 const { Title } = Typography;
 
-const expandable = { expandedRowRender: (record) => <p>{record.email}</p> };
+// const expandable = { expandedRowRender: (record) => <p>{record.password}</p> };
 const title = () => "User List";
 const showHeader = true;
 const footer = () => "BT Inc";
-const pagination = { position: "bottom" };
+const pagination = { position: ["none", "bottomCenter"] };
+const expandableContainer = { expandedRowRender: (record) => <p>{record.password}</p> };
 
 export default function UserPage(props) {
   const dispatch = useDispatch();
@@ -82,6 +84,7 @@ export default function UserPage(props) {
   };
 
   //===================================================================================
+  const [rowSelection, setRowSelection] = useState(false);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const hasSelected = selectedRowKeys.length > 0;
 
@@ -91,7 +94,7 @@ export default function UserPage(props) {
     setSelectedRowKeys(selectedRowKeys);
   };
 
-  const rowSelection = {
+  const handleRowSelection = {
     selectedRowKeys,
     onChange: handleSelectChange,
   };
@@ -110,14 +113,17 @@ export default function UserPage(props) {
     {
       title: "First Name",
       dataIndex: "firstName",
+      ellipsis: true,
     },
     {
       title: "Last Name",
       dataIndex: "lastName",
+      ellipsis: true,
     },
     {
       title: "Email",
       dataIndex: "email",
+      ellipsis: false,
       sorter: (a, b) => a.email > b.email,
       filters: [
         {
@@ -127,13 +133,14 @@ export default function UserPage(props) {
       ],
       onFilter: (value, record) => record.email.indexOf(value) === 0,
     },
-    {
-      title: "Password",
-      dataIndex: "password",
-    },
+    // {
+    //   title: "Password",
+    //   dataIndex: "password",
+    // },
     {
       title: "Action",
       dataIndex: "action",
+      ellipsis: true,
       fixed: "right",
       render: (user) => (
         <Space>
@@ -148,20 +155,71 @@ export default function UserPage(props) {
   const scroll = { x: "fixed" };
   // const scroll = { x: "100vw" };
 
+  const openNotificationWithIcon = (type) => {
+    console.log(type);
+
+    notification[type]({
+      message: "Users",
+      description: "User information management page",
+    });
+  };
+
+  // const [expandable, setExpandable] = useState(expandableContainer);
+  const [expandable, setExpandable] = useState(undefined);
+
+  const onChange = (event) => {
+    console.log(event.target.checked);
+
+    switch (event.target.name) {
+      case "Checkbox":
+        if (!event.target.checked) {
+          setSelectedRowKeys([]);
+        }
+
+        setRowSelection(event.target.checked);
+        break;
+      case "Expandable":
+        setExpandable(expandable ? undefined : expandableContainer);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const content = (
+    <div>
+      <Checkbox name="Checkbox" defaultChecked={expandable ? true : false} onChange={onChange}>
+        Checkbox
+      </Checkbox>
+      <Checkbox name="Expandable" defaultChecked={false} onChange={onChange}>
+        Expandable
+      </Checkbox>
+    </div>
+  );
+
   return (
     <div style={{ width: "100%", margin: "1rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "0rem" }}>
-        <Title level={2}>Users</Title>
+        <Title level={2} onClick={openNotificationWithIcon.bind(this, "success")}>
+          Users
+        </Title>
       </div>
-      {hasSelected && (
-        <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end", alignItems: "center", marginBottom: "0.25rem" }}>
-          <span style={{ margin: "0.25rem" }}>{hasSelected ? `Selected ${selectedRowKeys.length} items` : ""}</span>
-          <Button style={{ margin: "0rem" }} type="primary" disabled={!hasSelected} loading={false} onClick={handleClear}>
-            Clear
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: selectedRowKeys.length !== 0 ? "space-between" : "flex-end", alignItems: "center", marginBottom: "0.25rem" }}>
+        {hasSelected && rowSelection && (
+          <Space>
+            <Button style={{ margin: "0rem" }} type="primary" disabled={!hasSelected} loading={false} onClick={handleClear}>
+              Clear
+            </Button>
+            <span style={{ margin: "0.25rem" }}>{hasSelected ? `Selected ${selectedRowKeys.length} items` : ""}</span>
+          </Space>
+        )}
+        <Popover content={content} title="Option">
+          <Button type="primary" style={{ backgroundColor: green[6], borderColor: green[6] }}>
+            Option
           </Button>
-        </div>
-      )}
-      <Table rowSelection={rowSelection} columns={columns} dataSource={users} loading={loading} bordered={false} size="small" expandable={expandable} title={title} pagination={{ position: ["none", "bottomCenter"] }} showHeader={showHeader} footer={footer} scroll={scroll} />
+        </Popover>
+      </div>
+      <Table loading={loading} columns={columns} dataSource={users} rowSelection={rowSelection ? handleRowSelection : null} bordered={false} size="small" expandable={expandable} title={title} pagination={pagination} showHeader={showHeader} footer={footer} scroll={scroll} />
     </div>
   );
 }
